23cc9945422da19e98e76ff09457e4bce8f5b554
Jan Willem de Wit <jwdwit@gmail.com>
2017-05-04 14:10:43 +0200

    changes, added test file

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,9 +1,11 @@
+import org.omg.SendingContext.RunTime;
+
 import java.util.Random;
 
 /**
  * Created by jw on 11-3-17.
  */
-public class Matrix {
+public class Matrix implements java.io.Serializable{
     private int m; //rows
     private int n; //columns
     public double[][] data;
@@ -33,6 +35,48 @@ public class Matrix {
         return A;
     }
 
+    public static Matrix ones(int m, int n) {
+        Matrix A = new Matrix(m, n);
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                A.data[i][j] = 1.0;
+        return A;
+    }
+
+    public static Matrix random01(int m, int n) {
+        Random random = new Random();
+        Matrix A = new Matrix(m, n);
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                A.data[i][j] = random.nextDouble();
+        return A;
+    }
+
+    public static Matrix gaussian(int m, int n, double std) {
+        Random random = new Random();
+        Matrix A = new Matrix(m, n);
+        double factor = Math.sqrt(Math.PI/8);
+        double U = 0.0;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+            {
+                do {
+                    U = random.nextDouble(); //uniform [0,1]
+                } while (U==0.0||U==1.0);
+                A.data[i][j] = std*factor*Math.log(U/(1-U));
+            }
+        return A;
+    }
+
+    public Matrix narrow01() {
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++){
+                if(this.data[i][j]<0) this.data[i][j] = 0;
+                if(this.data[i][j]>1) this.data[i][j] = 1;
+            }
+        return this;
+    }
+
     public Matrix transpose() {
         Matrix A = new Matrix(n, m);
         for (int i = 0; i < m; i++)
@@ -44,7 +88,9 @@ public class Matrix {
     // return C = A + B
     public Matrix add(Matrix B) {
         Matrix A = this;
-        if (B.m != A.m || B.n != A.n) throw new RuntimeException("Illegal matrix dimensions.");
+        if (B.m != A.m || B.n != A.n) {
+            throw new RuntimeException("Illegal matrix dimensions.");
+        }
         Matrix C = new Matrix(m, n);
         for (int i = 0; i < m; i++)
             for (int j = 0; j < n; j++)
@@ -77,13 +123,21 @@ public class Matrix {
     // return C = A * B
     public Matrix multiply(Matrix B) {
         Matrix A = this;
-        if (A.n != B.m) throw new RuntimeException("Illegal matrix dimensions.");
+        if (A.n != B.m){
+            throw new RuntimeException("Illegal matrix dimensions.");
+        }
         Matrix C = new Matrix(A.m, B.n);
         for (int i = 0; i < C.m; i++)
             for (int j = 0; j < C.n; j++)
                 for (int k = 0; k < A.n; k++)
                     C.data[i][j] += (A.data[i][k] * B.data[k][j]);
         return C;
+    }
+
+    public void printSize(){
+        System.out.print(this.m);
+        System.out.print(" ");
+        System.out.println(this.n);
     }
 
     public Matrix multiply(double a) {
@@ -110,6 +164,7 @@ public class Matrix {
     }
 
     public void print() {
+        System.out.println("Start of matrix");
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(data[i][j]);
@@ -117,6 +172,7 @@ public class Matrix {
             }
             System.out.println();
         }
+        System.out.println("End of matrix");
         System.out.println();
     }
 
@@ -160,6 +216,7 @@ public class Matrix {
         return B;
     }
 
+
     public Matrix multiplyElementwise(Matrix B){
         Matrix A = this;
         if (B.m != A.m || B.n != A.n) throw new RuntimeException("Illegal matrix dimensions.");
@@ -169,6 +226,30 @@ public class Matrix {
                 C.data[i][j] = A.data[i][j] * B.data[i][j];
         return C;
 
+    }
+
+    public static Matrix concat(Matrix A, Matrix B){ //function for combining two matrices with the same amount of columns
+        if (A.n != B.n) throw new RuntimeException("Amount of columns is not the same");
+        Matrix C = new Matrix(A.m + B.m, A.n);
+        for (int i = 0; i < A.m; i++){
+            for (int j = 0; j < C.n; j++) {
+                C.data[i][j] = A.data[i][j];
+            }
+        }
+        for (int i = 0; i < B.m; i++){
+            for (int j = 0; j < C.n; j++){
+                C.data[i+A.m][j] = B.data[i][j];
+            }
+        }
+        return C;
+    }
+
+    public void set(int m, int n, double value){
+        this.data[m][n] = value;
+    }
+
+    public double get(int m, int n){
+        return this.data[m][n];
     }
 
 }
--- src/mlp.java
+++ src/mlp.java
@@ -6,136 +6,221 @@ import java.util.ArrayList;
 
 
 
-public class mlp {
+public class mlp implements java.io.Serializable {
+    public double learn_rate = 0.0005;
+    public double mean_weight = 0;
+    public double weight_spread = 2;
+
+    public int n_input;
+    public int n_hidden = 200;
+    public int n_output = 1;
+
+    public double noise_level = 0.01;
+    public double bias_value = -1.0;
+
+    public enum activationFunction {
+        LINEAR, SIGMOID
+    }
+    public activationFunction actFunc;
+
+    Matrix w_hidden;
+    Matrix w_output;
 
 
-    public static void main(String[] args){
-        double[][] mat = { { 1, 2, 3 }, { 4, 5, 6 }};
-        Matrix A = new Matrix(mat);
+    public mlp(int n_input){
+        //Randomly initialise weights
+        w_hidden = Matrix.random(n_input + 1, n_hidden);
+        w_output = Matrix.random(n_hidden, n_output);
+        w_hidden = w_hidden.multiply(weight_spread - (weight_spread / 2) + mean_weight);
+        w_output = w_output.multiply(weight_spread - (weight_spread / 2) + mean_weight);
+        this.actFunc = activationFunction.SIGMOID;
 
-        Matrix D = new Matrix(new double[][] {{1,2,3,4}});
-        Matrix E = new Matrix(new double[][] {{1,2,3,4}});
+        this.n_input = n_input;
+    }
+    public mlp(int n_input, activationFunction actFunc){
+        //Randomly initialise weights
+        w_hidden = Matrix.random(n_input + 1, n_hidden);
+        w_output = Matrix.random(n_hidden, n_output);
+        w_hidden = w_hidden.multiply(weight_spread - (weight_spread / 2) + mean_weight);
+        w_output = w_output.multiply(weight_spread - (weight_spread / 2) + mean_weight);
+        this.actFunc = actFunc;
 
-        Matrix F = D.multiplyElementwise(E);
-        //F.print();
+        this.n_input = n_input;
+    }
 
+    public mlp(int n_input, double learn_rate){
+        //Randomly initialise weights
+        w_hidden = Matrix.random(n_input + 1, n_hidden);
+        w_output = Matrix.random(n_hidden, n_output);
+        w_hidden = w_hidden.multiply(weight_spread - (weight_spread / 2) + mean_weight);
+        w_output = w_output.multiply(weight_spread - (weight_spread / 2) + mean_weight);
+        this.actFunc = activationFunction.SIGMOID;
 
-        Matrix examples = new Matrix(new double[][] {{0,0},{1,0},{0,1},{1,1}});
+        this.n_input = n_input;
+        this.learn_rate = learn_rate;
+    }
+    public mlp(int n_input, activationFunction actFunc, double learn_rate){
+        //Randomly initialise weights
+        w_hidden = Matrix.random(n_input + 1, n_hidden);
+        w_output = Matrix.random(n_hidden, n_output);
+        w_hidden = w_hidden.multiply(weight_spread - (weight_spread / 2) + mean_weight);
+        w_output = w_output.multiply(weight_spread - (weight_spread / 2) + mean_weight);
+        this.actFunc = actFunc;
 
-        Matrix goal = new Matrix(new double[][] {{0.01},{0.99},{0.99},{0.01}});
-
-        double learn_rate = 0.2;
-        int max_epoch = 5000;
-
-        double mean_weight = 0;
-        double weight_spread = 5;
-
-        int n_input = examples.numCols();
-        int n_hidden = 20;
-        int n_output = goal.numCols();
-
-        double noise_level = 0.01;
-        double bias_value = -1.0;
-
-        Matrix w_hidden = Matrix.random(n_input + 1, n_hidden);
-        w_hidden = w_hidden.multiply(weight_spread - (weight_spread/2) + mean_weight);
-
-        Matrix w_output = Matrix.random(n_hidden, n_output);
-        w_output = w_output.multiply(weight_spread - (weight_spread/2) + mean_weight);
-
-        double min_error = 0.01;
-        boolean stop_criterium = false;
-        int epoch = 0;
-
-        ArrayList h_error = new ArrayList();
-
-        while(!stop_criterium){
-            epoch += 1;
-
-            //noise not correct yet
-            Matrix noise = Matrix.random(examples.numRows(),examples.numCols());
-            noise = noise.multiply(noise_level);
-
-            Matrix input_data = examples.add(noise);
-
-            input_data = input_data.addBias(bias_value);
-            //input_data.print();
-
-            double epoch_error = 0;
-            double epoch_delta_hidden = 0;
-            double epoch_delta_output = 0;
-
-            for (int pattern = 0; pattern < input_data.numRows(); pattern++){
-
-                Matrix hidden_activation = input_data.getRow(pattern);
-                hidden_activation = hidden_activation.multiply(w_hidden);
-
-                Matrix hidden_output = hidden_activation.sigmoid();
-
-                Matrix output_activation = hidden_output.multiply(w_output);
-
-                //1x1 matrix
-                Matrix outputM = output_activation.sigmoid();
-                double output = outputM.data[0][0];
-
-                double output_error = goal.data[pattern][0] - output;
-
-                Matrix local_gradient_output = output_activation.d_sigmoid();
-                local_gradient_output = local_gradient_output.multiply(output_error);
-
-                Matrix hidden_error = w_output.multiply(local_gradient_output.data[0][0]);
-
-                Matrix local_gradient_hidden = hidden_activation.d_sigmoid();
-                local_gradient_hidden = local_gradient_hidden.multiplyElementwise(hidden_error.transpose());
+        this.n_input = n_input;
+        this.learn_rate = learn_rate;
+    }
 
 
-                Matrix delta_output = hidden_output.multiply(local_gradient_output.data[0][0]);
-                delta_output = delta_output.multiply(learn_rate);
 
+    public void train(Matrix examples, Matrix goal, double learning_factor) {
 
-                Matrix delta_hidden = input_data.getRow(pattern).transpose().multiply(local_gradient_hidden);
-                delta_hidden = delta_hidden.multiply(learn_rate);
+        //Matrix examples = new Matrix(new double[][] {{0,0},{1,0},{0,1},{1,1}});
+        //Matrix goal = new Matrix(new double[][] {{0.01},{0.99},{0.99},{0.01}});
 
-                w_hidden = w_hidden.add(delta_hidden);
+        Matrix noise = Matrix.random(examples.numRows(), examples.numCols());
+        noise = noise.multiply(noise_level);
 
-                w_output = w_output.add(delta_output.transpose());
+        Matrix input_data = examples.add(noise);
 
-                epoch_error += output_error * output_error;
+        input_data = input_data.addBias(bias_value);
+        //input_data.print();
 
-            }
-
-            h_error.add(epoch_error / input_data.numRows());
-
-            if (epoch >= max_epoch || epoch_error < min_error){
-                stop_criterium = true;
-            }
-
-        }
-        System.out.println(epoch);
-        //System.out.println(h_error);
-
-
-        for (int pattern = 0; pattern < examples.numRows(); pattern++) {
-            Matrix input_data = examples.addBias(bias_value);
+        for (int pattern = 0; pattern < input_data.numRows(); pattern++) {
+            //System.out.println(pattern);
 
             Matrix hidden_activation = input_data.getRow(pattern);
+
             hidden_activation = hidden_activation.multiply(w_hidden);
 
-            Matrix hidden_output = hidden_activation.sigmoid();
+            Matrix hidden_output;
+            switch (actFunc){
+                case SIGMOID:
+                    hidden_output = hidden_activation.sigmoid();
+                    break;
+                case LINEAR:
+                    hidden_output =  hidden_activation.sigmoid();
+                    break;
+                default:
+                    hidden_output = hidden_activation.sigmoid();
+                    break;
+            }
 
             Matrix output_activation = hidden_output.multiply(w_output);
 
             //1x1 matrix
-            Matrix outputM = output_activation.sigmoid();
+            Matrix outputM;
+            switch (actFunc){
+                case SIGMOID:
+                    outputM = output_activation.sigmoid();
+                    break;
+                case LINEAR:
+                    outputM = output_activation;
+                    break;
+                default:
+                    outputM = output_activation.sigmoid();
+                    break;
+            }
+
             double output = outputM.data[0][0];
 
-            System.out.println("Input: ");
-            examples.getRow(pattern).print();
-            System.out.println("Output: ");
-            System.out.println(output);
-            System.out.println();
+            double output_error = goal.data[pattern][0] - output;
+
+            Matrix local_gradient_output;
+            switch (actFunc){
+                case LINEAR: //fill with zeroes
+                    local_gradient_output = Matrix.ones(output_activation.numRows(), output_activation.numCols());
+                    break;
+                case SIGMOID:
+                    local_gradient_output = output_activation.d_sigmoid();
+                    break;
+                default:
+                    local_gradient_output = output_activation.d_sigmoid();
+                    break;
+            }
+
+            local_gradient_output = local_gradient_output.multiply(output_error);
+
+            Matrix hidden_error = w_output.multiply(local_gradient_output.data[0][0]);
+
+            Matrix local_gradient_hidden;
+            switch (actFunc){
+                case LINEAR: //fill with zeroes
+                    local_gradient_hidden = hidden_activation.d_sigmoid();
+                    break;
+                case SIGMOID:
+                    local_gradient_hidden = hidden_activation.d_sigmoid();
+                    break;
+                default:
+                    local_gradient_hidden = hidden_activation.d_sigmoid();
+                    break;
+            }
+
+
+            local_gradient_hidden = local_gradient_hidden.multiplyElementwise(hidden_error.transpose());
+
+
+            Matrix delta_output = hidden_output.multiply(local_gradient_output.data[0][0]);
+            delta_output = delta_output.multiply(learn_rate*learning_factor);
+
+            Matrix delta_hidden = input_data.getRow(pattern).transpose().multiply(local_gradient_hidden);
+            delta_hidden = delta_hidden.multiply(learn_rate*learning_factor);
+
+            w_hidden = w_hidden.add(delta_hidden);
+
+            w_output = w_output.add(delta_output.transpose());
+            //delta_output.transpose().print();
+
         }
+
+
     }
+
+    public Matrix output(Matrix input){
+        Matrix input_data = input.addBias(bias_value);
+
+        Matrix hidden_activation = input_data;
+        hidden_activation = hidden_activation.multiply(w_hidden);
+
+
+        Matrix hidden_output;
+        switch (actFunc){
+            case SIGMOID:
+                hidden_output = hidden_activation.sigmoid();
+                break;
+            case LINEAR:
+                hidden_output =  hidden_activation.sigmoid();
+                break;
+            default:
+                hidden_output = hidden_activation.sigmoid();
+                break;
+        }
+
+        //hidden_output.print();
+
+        Matrix output_activation = hidden_output.multiply(w_output);
+
+        //1x1 matrix
+        Matrix outputM;
+        switch (actFunc){
+            case SIGMOID:
+                outputM = output_activation.sigmoid();
+                break;
+            case LINEAR:
+                outputM = output_activation;
+                break;
+            default:
+                outputM = output_activation.sigmoid();
+                break;
+        }
+        //outputM.print();
+        //double output = outputM.data[0][0];
+        return outputM;
+    }
+
+
+
+
 }
 
 
